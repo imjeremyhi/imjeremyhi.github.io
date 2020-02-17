ccd138e956631983db210aa99f1077fb43010d30
Jeremy Fu <jfu@atlassian.com>
2020-02-17 22:38:22 +1100

    Use build contents

--- README.md
+++
@@ -1,3 +0,0 @@
-# Personal Website
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ precache-manifest.f0ebd8d0cb8b7541bae1f06a66b98770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a525f871287d5d9bbf51c15d7e759416",
+    "url": "/index.html"
+  },
+  {
+    "revision": "515e09e9b114047277ac",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2681fd59f71b22f87924",
+    "url": "/static/js/2.08737a87.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.08737a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "515e09e9b114047277ac",
+    "url": "/static/js/main.f2edb026.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
