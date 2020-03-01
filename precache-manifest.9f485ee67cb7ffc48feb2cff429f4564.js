893311a61b70fd475e637170385dd68b1082f6a1
Jeremy Fu <imjeremyhi@gmail.com>
2020-03-01 12:28:34 +1100

    Make paths relative

---
+++ precache-manifest.9f485ee67cb7ffc48feb2cff429f4564.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e764237c6e4372d3ad4ae4369667e1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5588357b952aa3386377",
+    "url": "/static/css/2.3eb7968f.chunk.css"
+  },
+  {
+    "revision": "c00a44728d282f74d20d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5588357b952aa3386377",
+    "url": "/static/js/2.ba23fa5e.chunk.js"
+  },
+  {
+    "revision": "6dacdc65fe1a8c6a031ffd1bd7da1c21",
+    "url": "/static/js/2.ba23fa5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c00a44728d282f74d20d",
+    "url": "/static/js/main.b1949718.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
