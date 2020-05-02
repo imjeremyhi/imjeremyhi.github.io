928ea5df844f6a0e4bbae5319ef174efe6e17753
Jeremy Fu <imjeremyhi@gmail.com>
2020-05-02 13:12:12 +1000

    Use firebase functions for storing to database

---
+++ precache-manifest.46fa68b999b47fc38ab855600d741853.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5924669e5336aea5342d9a2d25d79d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae74c03b2a8c4f26ca2d",
+    "url": "/static/css/2.3eb7968f.chunk.css"
+  },
+  {
+    "revision": "da39d6fd174b0e90b230",
+    "url": "/static/css/main.5d8039e5.chunk.css"
+  },
+  {
+    "revision": "ae74c03b2a8c4f26ca2d",
+    "url": "/static/js/2.148aa905.chunk.js"
+  },
+  {
+    "revision": "b4a18934c7af64116bc49bf1c41a9749",
+    "url": "/static/js/2.148aa905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da39d6fd174b0e90b230",
+    "url": "/static/js/main.e59d9780.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
