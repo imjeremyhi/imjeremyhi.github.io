bd6c89d65814baa92e39dd50f5abf9c94c3ba421
Jeremy Fu <imjeremyhi@gmail.com>
2020-05-28 00:03:13 +1000

    Fix hero animation

---
+++ precache-manifest.049b5d61ed6ea252698b7f1a316ec0af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132fa15a41252da2ba5bcdaff11da1b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13e536a75dbc0ef383b9",
+    "url": "/static/css/2.3eb7968f.chunk.css"
+  },
+  {
+    "revision": "20917d75113da47f59b9",
+    "url": "/static/css/main.5d8039e5.chunk.css"
+  },
+  {
+    "revision": "13e536a75dbc0ef383b9",
+    "url": "/static/js/2.a6431444.chunk.js"
+  },
+  {
+    "revision": "8c9fd5c8d823d981c171b2083dde7287",
+    "url": "/static/js/2.a6431444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20917d75113da47f59b9",
+    "url": "/static/js/main.87a608a2.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
