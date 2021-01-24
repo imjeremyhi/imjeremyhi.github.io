3a9e5572810d975981467078120525e88fce39c4
Jeremy Fu <jeremyfu@fb.com>
2021-01-24 10:06:40 -0500

    Clean up sections for project website

---
+++ precache-manifest.5674b84bc535f0d799f1a8253bc73ffd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622561c9ca10f365f1a3150b3c2b327d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "091b8482a8024698b7df",
+    "url": "/static/css/2.3eb7968f.chunk.css"
+  },
+  {
+    "revision": "e13f6d6717e62b4bf479",
+    "url": "/static/css/main.5d8039e5.chunk.css"
+  },
+  {
+    "revision": "091b8482a8024698b7df",
+    "url": "/static/js/2.66952b36.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.66952b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13f6d6717e62b4bf479",
+    "url": "/static/js/main.8bf5beb0.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b454d43f1305b3338d40f41d6f1dadb.js"
+  "/precache-manifest.5674b84bc535f0d799f1a8253bc73ffd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
